73f7587a9f6188c87fbc8d5468279f2ca1dff51c
I3ene <benedikt.muell@t-online.de>
2022-10-14 13:48:35 +0200

    Renamed project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/card-game'),
+      dir: require('path').join(__dirname, './coverage/decksterous'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'card-game'`, () => {
+  it(`should have as title 'decksterous'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('card-game');
+    expect(app.title).toEqual('decksterous');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('card-game app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('decksterous app is running!');
   });
 });
