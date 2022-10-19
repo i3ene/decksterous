0b03716e6c5221326ef99617b95e74a12b854282
I3ene <benedikt.muell@t-online.de>
2022-10-19 15:34:46 +0200

    Added Register Form

---
+++ src/app/components/forms/register-form/register-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
