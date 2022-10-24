45385b931b7dba8256c9966f722f8c781f97ed71
Benedikt Müll <benedikt.muell@zimmer-group.com>
2022-10-24 13:44:29 +0200

    Added Auth Guard

---
+++ src/app/services/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Config } from 'src/app/config/config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkToken();
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkToken();
+  }
+
+  checkToken(): boolean {
+    const token = localStorage[Config.AuthToken];
+    if (!token) this.router.navigate(["/auth"]);
+    return !!token;
+  }
+}
