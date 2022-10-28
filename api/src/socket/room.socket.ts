609b406531a62be75eec3592c7519feaf9631ecf
Benedikt Müll <benedikt.muell@zimmer-group.com>
2022-10-28 13:55:54 +0200

    Extended Lobby functionality

--- api/src/socket/room.socket.ts
+++ api/src/socket/room.socket.ts
@@ -2,20 +2,31 @@ import { Server, Socket } from 'socket.io';
 
 export namespace RoomSocket {
   export async function register(io: Server, socket: Socket) {
+    // Retrieve all current rooms
     socket.emit('room_list', await getRooms(io));
 
+    // Join room
     socket.on('room_join', async (room) => {
       socket.join(room);
-      io.to(room).emit('room_socket_join', { socket: socket.id, room: room });
+      io.to(room).emit('room_socket_join', { socket: socket.id, room: room, action: "joined" });
       var list = await await getSockets(io, room);
       io.to(room).emit('room_socket_list', { [room]: list });
+
+      // Listen to specific room events
+      socket.on("room_event_" + room, async (event) => {
+        io.to(room).emit("room_socket_event_" + room, event);
+      });
     });
 
+    // Leave room
     socket.on('room_leave', async (room) => {
+      io.to(room).emit('room_socket_leave', { socket: socket.id, room: room, action: "left" });
       socket.leave(room);
-      io.to(room).emit('room_socket_leave', { socket: socket.id, room: room });
       var list = await getSockets(io, room);
       io.to(room).emit('room_socket_list', { [room]: list });
+
+      // Remove room events
+      socket.removeAllListeners("room_event_" + room);
     });
   }
 
