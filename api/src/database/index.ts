41c7c5ab10690a7eb54adc47eb4dec8559e88543
Oliver Saar <oli.saar123@gmail.com>
2022-10-29 13:19:35 +0200

    integrated models in sequelize

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,9 +1,12 @@
-import { Inventory } from "../models/data/inventory.model";
-import { InventoryItem } from "../models/data/inventory_item.model";
-import { Item } from "../models/data/item.model";
-import { User } from "../models/data/user.model";
-import { SequelizeDatabase } from "./sequelize.database";
+import {Inventory} from "../models/data/inventory.model";
+import {InventoryItem} from "../models/data/inventory_item.model";
+import {Item} from "../models/data/item.model";
+import {User} from "../models/data/user.model";
+import {SequelizeDatabase} from "./sequelize.database";
+import {Card} from "../models/data/card.model";
+import {CardType} from "../models/data/cardType.model";
+import {CardItem} from "../models/data/card_item.model"
 
-SequelizeDatabase.addModels([User, Inventory, Item, InventoryItem]);
+SequelizeDatabase.addModels([User, Inventory, Item, InventoryItem, Card, CardType, CardItem]);
 
 export const database = SequelizeDatabase;
--- api/src/server/express.server.ts
+++ api/src/server/express.server.ts
@@ -1,16 +1,19 @@
 import express from 'express';
-import { AuthMiddleware } from '../middleware/auth.middleware';
-import { HeaderMiddleware } from '../middleware/header.middleware';
-import { AuthRoutes } from '../routes/auth.routes';
-import { UserRoutes } from '../routes/user.routes';
+import {AuthMiddleware} from '../middleware/auth.middleware';
+import {HeaderMiddleware} from '../middleware/header.middleware';
+import {AuthRoutes} from '../routes/auth.routes';
+import {UserRoutes} from '../routes/user.routes';
+import {CardRoutes} from "../routes/card.routes";
 
 export const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(HeaderMiddleware.handler);
 
 app.use('/auth', AuthRoutes);
 
 app.use(AuthMiddleware.verifyToken);
 app.use('/user', UserRoutes);
+
+app.use('/card', CardRoutes);
