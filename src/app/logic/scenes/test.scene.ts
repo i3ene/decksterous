2c967345b74d905d67cffac30006f8710f4f3a54
I3ene <benedikt.muell@t-online.de>
2022-10-30 16:03:17 +0100

    Added tween.js functionality

--- src/app/logic/scenes/test.scene.ts
+++ src/app/logic/scenes/test.scene.ts
@@ -2,8 +2,9 @@ import { CubeThree } from 'src/app/models/cube.three';
 import { PlaneThree } from 'src/app/models/plane.three';
 import { IScene } from 'src/app/models/scene.model';
 import * as THREE from 'three';
-import { Camera, DirectionalLight, HemisphereLight, Mesh } from 'three';
+import { Camera, DirectionalLight, HemisphereLight, Mesh, Vector3 } from 'three';
 import { ThreeLogic } from '../three.logic';
+import * as TWEEN from '@tweenjs/tween.js';
 
 export class TestScene implements IScene {
   ambient!: HemisphereLight;
@@ -45,11 +46,33 @@ export class TestScene implements IScene {
     threeLogic.scene.add(this.cube);
     threeLogic.scene.add(this.plane);
     this.camerSetup(threeLogic.camera);
-    threeLogic.renderList.push(this.cubeAnimation.bind(this));
+    threeLogic.loopFunctions.push(this.cubeRotation.bind(this));
+    threeLogic.loopFunctions.push(this.cubeCircle.bind(this));
+    //this.cubeMove(threeLogic);
   }
 
-  cubeAnimation() {
+  cubeRotation(threeLogic: ThreeLogic) {
     this.cube.rotation.x += 0.003;
     this.cube.rotation.y += 0.007;
+  }
+
+  cubeCircle(threeLogic: ThreeLogic) {
+    this.cube.position.x = Math.sin(threeLogic.time.current * 0.002) * 2;
+    this.cube.position.y = Math.sin(threeLogic.time.current * 0.001) * 1 + 2;
+    this.cube.position.z = Math.cos(threeLogic.time.current * 0.002) * 2;
+  }
+
+  cubeMove(threeLogic: ThreeLogic) {
+    var move1 = threeLogic.move(this.cube, new Vector3(1, 1, 0), 20).easing(TWEEN.Easing.Cubic.Out);
+    var move2 = threeLogic.move(this.cube, new Vector3(0, -2, -1), 40).easing(TWEEN.Easing.Cubic.Out);
+    var move3 = threeLogic.move(this.cube, new Vector3(-2, 0, 2), 40).easing(TWEEN.Easing.Cubic.Out);
+    var move4 = threeLogic.move(this.cube, new Vector3(1, 1, -1), 20).easing(TWEEN.Easing.Cubic.Out);
+
+    move1.chain(move2);
+    move2.chain(move3);
+    move3.chain(move4);
+    move4.chain(move1);
+
+    move1.start();
   }
 }
--- src/app/logic/three.logic.ts
+++ src/app/logic/three.logic.ts
@@ -1,9 +1,10 @@
-import { ElementRef } from "@angular/core";
-import { IScene } from "src/app/models/scene.model";
-import { Color, PerspectiveCamera, Scene, TextureLoader, WebGLRenderer } from "three";
+import { ElementRef } from '@angular/core';
+import { IScene } from 'src/app/models/scene.model';
+import { Color, Mesh, Path, PerspectiveCamera, Scene, TextureLoader, Vector3, WebGLRenderer } from 'three';
+import * as TWEEN from '@tweenjs/tween.js';
+import { Tween } from '@tweenjs/tween.js';
 
 export class ThreeLogic {
-
   private canvasRef!: ElementRef;
 
   /** Stage Properties **/
@@ -18,20 +19,21 @@ export class ThreeLogic {
 
   /** Helper Properties **/
 
+  public time: { delta: number; current: number, previous: number } = { delta: 0, current: 0, previous: 0 };
+
   public camera!: PerspectiveCamera;
 
   private get canvas(): HTMLCanvasElement {
     return this.canvasRef.nativeElement;
   }
 
-  private loader = new TextureLoader;
+  private loader = new TextureLoader();
 
   private renderer!: WebGLRenderer;
 
   public scene!: Scene;
 
-  public renderList: { (): void }[] = [];
-
+  public loopFunctions: { (threeLogic: ThreeLogic): void }[] = [];
 
   /** Constructor **/
 
@@ -44,14 +46,9 @@ export class ThreeLogic {
   public createScene() {
     this.scene = new Scene();
     this.scene.background = new Color(0x000000);
-    
+
     let aspectRatio = this.getAspectRatio();
-    this.camera = new PerspectiveCamera(
-      this.fieldOfView,
-      aspectRatio,
-      this.nearClippingPlane,
-      this.farClippingPlane
-    )
+    this.camera = new PerspectiveCamera(this.fieldOfView, aspectRatio, this.nearClippingPlane, this.farClippingPlane);
     this.camera.position.z = this.cameraZ;
     this.camera.castShadow = true;
   }
@@ -69,17 +66,49 @@ export class ThreeLogic {
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
     let component: ThreeLogic = this;
-    (function render() {
+    (function render(time: number) {
+      component.time.current = time;
+      component.time.delta = time - component.time.previous;
+      component.time.previous = time;
+
       requestAnimationFrame(render);
-      component.renderList.forEach(fn => fn());
+      TWEEN.update(time * 0.1);
+      component.loopFunctions.forEach((fn) => fn(component));
       component.renderer.render(component.scene, component.camera);
-    }());
+    })(component.time.previous);
   }
+
+  /** Engine Setup **/
 
   public registerScene(scene: IScene) {
     this.createScene();
     this.startRenderingLoop();
     scene.bind(this);
   }
-  
+
+  /** Functions **/
+
+  public move(position: Vector3 | Mesh, to: Vector3 | Path, speed: number, translate: boolean = true) {
+    const from = position instanceof Mesh ? position.position : position;
+    if (to instanceof Vector3) {
+      const duration = from.distanceTo(to) * 1000 / speed;
+      let origin = from.clone();
+      const tween = new TWEEN.Tween(translate ? new Vector3(0, 0, 0) : from);
+      if (translate) tween.to(to, duration).onUpdate(vec => {
+        from.x = vec.x + origin.x;
+        from.y = vec.y + origin.y;
+        from.z = vec.z + origin.z;
+      });
+      else tween.to(to, duration)
+      tween.onStart(vec => {
+        origin = from.clone();
+      });
+      tween.delay(0).repeatDelay(0);
+      return tween;
+    } else {
+      // TODO: Path Logic
+      return new TWEEN.Tween(from);
+    }
+  }
+
 }
