858a3efa65918e63537fc8cbc73c20d329c57f1d
I3ene <benedikt.muell@t-online.de>
2022-11-03 12:37:52 +0100

    Added CardDeck Model

---
+++ api/src/models/data/cardDeck.model.ts
@@ -0,0 +1,26 @@
+import {AllowNull, AutoIncrement, BelongsTo, BelongsToMany, Column, DataType, ForeignKey, HasMany, Model, PrimaryKey, Scopes, Table} from "sequelize-typescript";
+import {QueryUtil} from "../../utils/query.util";
+import { Card } from "./card.model";
+import { CardCardDeck } from "./card_cardDeck.model";
+import { Item } from "./item.model";
+
+@Scopes(() => ({
+  query: QueryUtil.query(['id', 'name', 'description']),
+  card: {
+    include: [Card]
+  }
+}))
+@Table
+export class CardDeck extends Model<CardDeck> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
+
+  @ForeignKey(() => Item)
+  @Column(DataType.INTEGER)
+  itemId!: number;
+
+  @BelongsToMany(() => Card, () => CardCardDeck)
+  cards?: Card[];
+}
---
+++ api/src/models/data/card_cardDeck.model.ts
@@ -0,0 +1,14 @@
+import {Column, DataType, ForeignKey, Model, Table} from 'sequelize-typescript';
+import { Card } from './card.model';
+import { CardDeck } from './cardDeck.model';
+
+@Table
+export class CardCardDeck extends Model<CardCardDeck> {
+  @ForeignKey(() => Card)
+  @Column(DataType.INTEGER)
+  cardId!: number;
+
+  @ForeignKey(() => CardDeck)
+  @Column(DataType.INTEGER)
+  cardDeckId!: number;
+}
