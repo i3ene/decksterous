aab0638a8d0479fb31d3599ebf4d9ac0c80a6244
I3ene <benedikt.muell@t-online.de>
2022-11-03 12:37:38 +0100

    Added CardAbility Model

---
+++ api/src/models/data/cardAbility.model.ts
@@ -0,0 +1,27 @@
+import {AllowNull, AutoIncrement, BelongsTo, BelongsToMany, Column, DataType, ForeignKey, Model, PrimaryKey, Scopes, Table} from "sequelize-typescript";
+import {QueryUtil} from "../../utils/query.util";
+import { Card } from "./card.model";
+import { CardCardAbility } from "./card_cardAbility.model";
+
+@Scopes(() => ({
+  query: QueryUtil.query(['id', 'name', 'description']),
+  card: {
+    include: [Card]
+  }
+}))
+@Table
+export class CardAbility extends Model<CardAbility> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
+
+  @Column(DataType.STRING(64))
+  name!: string;
+
+  @Column(DataType.STRING(255))
+  description!: string;
+
+  @BelongsToMany(() => Card, () => CardCardAbility)
+  cards?: Card[];
+}
