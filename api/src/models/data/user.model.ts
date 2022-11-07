cdecd9ad0d6aa0ea0a51d9ef210f0408f5cc2a0b
Benedikt Müll <benedikt.muell@zimmer-group.com>
2022-11-07 15:50:04 +0100

    Adjusted User Model

--- api/src/models/data/user.model.ts
+++ api/src/models/data/user.model.ts
@@ -1,43 +1,55 @@
-import { AutoIncrement, Column, DataType, Model, PrimaryKey, Table, Scopes, HasOne, BelongsToMany } from "sequelize-typescript";
+import { AutoIncrement, Column, DataType, Model, PrimaryKey, Table, Scopes, HasOne, BelongsToMany } from 'sequelize-typescript';
 import * as bcrypt from 'bcryptjs';
-import { Op } from "sequelize";
-import { QueryUtil } from "../../utils/query.util";
-import { Inventory } from "./inventory.model";
-import { Friend } from "./friend.model";
+import { Op } from 'sequelize';
+import { QueryUtil } from '../../utils/query.util';
+import { Inventory } from './inventory.model';
+import { Friend } from './friend.model';
 
 @Scopes(() => ({
-    query: QueryUtil.query(['id', 'name', 'mail']),
-    inventory: {
-        include: [Inventory]
-    },
-    friend: {
-        include: [User]
-    }
+  query: QueryUtil.query(['id', 'name', 'mail', 'xp']),
+  inventory: {
+    include: [Inventory],
+  },
+  friend: {
+    include: [User],
+  },
 }))
 @Table
 export class User extends Model<User> {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
-    id!: number;
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
 
-    @Column(DataType.STRING(32))
-    name!: string;
+  @Column(DataType.STRING(32))
+  name!: string;
 
-    @Column(DataType.STRING)
-    get password(): string {
-        return this.getDataValue('password');
-    };
-    set password(value: string) {
-        this.setDataValue('password', bcrypt.hashSync(value, 8));
-    };
+  @Column(DataType.STRING)
+  get password(): string {
+    return this.getDataValue('password');
+  }
+  set password(value: string) {
+    this.setDataValue('password', bcrypt.hashSync(value, 8));
+  }
 
-    @Column(DataType.STRING(128))
-    mail!: string;
+  @Column(DataType.STRING(128))
+  mail!: string;
 
-    @HasOne(() => Inventory)
-    inventory?: Inventory;
+  @Column(DataType.INTEGER)
+  xp!: number;
 
-    @BelongsToMany(() => User, () => Friend)
-    friends?: User[];
+  @Column(DataType.BLOB)
+  get avatar(): any {
+    const data = this.getDataValue('avatar');
+    return data ? data.toString('utf8') : '';
+  }
+  set avatar(value: any) {
+    this.setDataValue('avatar', value);
+  }
+
+  @HasOne(() => Inventory)
+  inventory?: Inventory;
+
+  @BelongsToMany(() => User, () => Friend)
+  friends?: User[];
 }
