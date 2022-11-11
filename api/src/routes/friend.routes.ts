5ef05dc3ee330bcd4b260edd8f0b73c545490ea9
Benedikt Müll <benedikt.muell@zimmer-group.com>
2022-11-11 11:01:50 +0100

    Created Friend Routes

---
+++ api/src/routes/friend.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { RequestController as controller } from "../controllers/request.controller";
+import { AuthMiddleware as auth } from "../middleware/auth.middleware";
+import { RequestMiddleware as middleware } from "../middleware/request.middleware";
+import { User } from "../models/data/user.model";
+
+export const FriendRoutes = Router();
+
+FriendRoutes.get("/", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('intersection', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
+
+FriendRoutes.post("/", [auth.isAdmin, middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.addAssociation(User, 'friends', 'friend')], controller.message("last"));
+
+FriendRoutes.delete("/", [auth.isAdmin, middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.removeAssociation(User, 'friends', 'friend')], controller.message('last'));
+
+FriendRoutes.get("/check", [middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.hasAssociation(User, 'friends', 'friend')], controller.result([User, 'friends']));
+
+FriendRoutes.get("/requests", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('right', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
+
+FriendRoutes.get("/invites", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('left', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -3,6 +3,7 @@ import { RequestController as controller } from "../controllers/request.controll
 import { AuthMiddleware as auth } from "../middleware/auth.middleware";
 import { RequestMiddleware as middleware } from "../middleware/request.middleware";
 import { User } from "../models/data/user.model";
+import { FriendRoutes } from "./friend.routes";
 
 export const UserRoutes = Router();
 
@@ -16,14 +17,4 @@ UserRoutes.put("/", [auth.isAdmin, middleware.get(User), middleware.edit(User)],
 
 UserRoutes.delete("/", [auth.isAdmin, middleware.get(User), middleware.remove(User)], controller.message("last"));
 
-UserRoutes.get("/friend", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('intersection', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
-
-UserRoutes.post("/friend", [ middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.addAssociation(User, 'friends', 'friend')], controller.message("last"));
-
-UserRoutes.delete("/friend", [ middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.removeAssociation(User, 'friends', 'friend')], controller.message('last'));
-
-UserRoutes.get("/friend/check", [ middleware.get(User), middleware.get(User, [], 'friend', 'friendId'), middleware.hasAssociation(User, 'friends', 'friend')], controller.result([User, 'friends']));
-
-UserRoutes.get("/friend/requests", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('right', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
-
-UserRoutes.get("/friend/invites", [middleware.get(User), middleware.getAssociation(User, 'friends'), middleware.getAssociation(User, 'requests'), middleware.difference('left', [User, 'requests'], [User, 'friends'], 'difference', 'id')], controller.result('difference'));
+UserRoutes.use("/friend", FriendRoutes);
