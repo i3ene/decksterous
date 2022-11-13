190e62af665af73d96916e7166cb06a2ef97a282
Oliver Saar <oli.saar123@gmail.com>
2022-11-13 17:21:37 +0100

    adjusted routing with middleware

--- api/src/routes/card.routes.ts
+++ api/src/routes/card.routes.ts
@@ -1,15 +1,18 @@
-import {Router} from "express";
+import { Router } from 'express';
 
-import { AuthMiddleware as auth } from "../middleware/auth.middleware";
+import { AuthMiddleware as auth } from '../middleware/auth.middleware';
+import { RequestMiddleware as middleware } from '../middleware/request.middleware';
+import { RequestController as controller } from '../controllers/request.controller';
+import { Card } from '../models/data/card.model';
 
 export const CardRoutes = Router();
 
-CardRoutes.get("/all", );
+CardRoutes.get('/all', [middleware.findAll(Card)], controller.result(Card));
 
-CardRoutes.get("/", );
+CardRoutes.get('/', [middleware.find(Card, ['items'])], controller.result(Card));
 
-CardRoutes.post("/", [auth.isAdmin], );
+CardRoutes.post('/', [auth.isAdmin, middleware.add(Card)], controller.message('last'));
 
-CardRoutes.put("/", [auth.isAdmin], );
+CardRoutes.put('/', [auth.isAdmin, middleware.get(Card), middleware.edit(Card)], controller.message('last'));
 
-CardRoutes.delete("/", [auth.isAdmin], );
+CardRoutes.delete('/', [auth.isAdmin, middleware.get(Card), middleware.remove(Card)], controller.message('last'));
--- api/src/routes/cardType.routes.ts
+++ api/src/routes/cardType.routes.ts
@@ -1,14 +1,17 @@
-import {Router} from "express";
-import { AuthMiddleware as auth } from "../middleware/auth.middleware";
+import { Router } from 'express';
+import { AuthMiddleware as auth } from '../middleware/auth.middleware';
+import { RequestMiddleware as middleware } from '../middleware/request.middleware';
+import { RequestController as controller } from '../controllers/request.controller';
+import { CardType } from '../models/data/cardType.model';
 
 export const CardTypeRoutes = Router();
 
-CardTypeRoutes.get("/all", );
+CardTypeRoutes.get('/all', [middleware.findAll(CardType)], controller.result(CardType));
 
-CardTypeRoutes.get("/", );
+CardTypeRoutes.get('/', [middleware.find(CardType, ['items'])], controller.result(CardType));
 
-CardTypeRoutes.post("/", [auth.isAdmin], );
+CardTypeRoutes.post('/', [auth.isAdmin, middleware.add(CardType)], controller.message('last'));
 
-CardTypeRoutes.put("/", [auth.isAdmin], );
+CardTypeRoutes.put('/', [auth.isAdmin, middleware.get(CardType), middleware.edit(CardType)], controller.message('last'));
 
-CardTypeRoutes.delete("/", [auth.isAdmin], );
+CardTypeRoutes.delete('/', [auth.isAdmin, middleware.get(CardType), middleware.remove(CardType)], controller.message('last'));
