42f55559ff2b5b8c4e710f41ed84318b938fc87e
I3ene <benedikt.muell@t-online.de>
2022-11-17 10:45:56 +0100

    Implemented Message Display

--- src/app/services/request/request.service.ts
+++ src/app/services/request/request.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { firstValueFrom } from 'rxjs';
 import { REQUEST_CONFIG, RequestConfig } from '../../config/request.config';
 
@@ -7,31 +8,36 @@ import { REQUEST_CONFIG, RequestConfig } from '../../config/request.config';
   providedIn: 'root',
 })
 export class RequestService {
-  constructor(@Inject(REQUEST_CONFIG) public config: RequestConfig, private http: HttpClient) {}
+  constructor(@Inject(REQUEST_CONFIG) public config: RequestConfig, private http: HttpClient, private dialog: MatSnackBar) {}
 
   get(url: string, options: any = { responseType: 'json' }): Promise<any> {
-    return firstValueFrom(this.http.get(this.config.url + url, options));
+    return firstValueFrom(this.http.get(this.config.url + url, options)).then(res => { this.display(res); return res });
   }
 
   post(url: string, body: any, options: any = { responseType: 'json' }): Promise<any> {
-    return firstValueFrom(this.http.post(this.config.url + url, body, options));
+    return firstValueFrom(this.http.post(this.config.url + url, body, options)).then(res => { this.display(res); return res });
   }
 
   put(url: string, body: any, options: any = { responseType: 'json' }): Promise<any> {
-    return firstValueFrom(this.http.put(this.config.url + url, body, options));
+    return firstValueFrom(this.http.put(this.config.url + url, body, options)).then(res => { this.display(res); return res });
   }
 
-  delete(url: string, body?: any /*, options: any*/): Promise<any> {
+  delete(url: string, body?: any, options: any = {}): Promise<any> {
     if (body) {
-      const options = {
+      options = {
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
         }),
         body: body,
       };
-      return firstValueFrom(this.http.delete(this.config.url + url, options));
-    } else {
-      return firstValueFrom(this.http.delete(this.config.url + url /*, options*/));
     }
+    return firstValueFrom(this.http.delete(this.config.url + url, options)).then(res => { this.display(res); return res });
+  }
+
+  display(response: any) {
+    if (!response.message) return;
+    this.dialog.open(response.message, "Ok", {
+      duration: 2500
+    });
   }
 }
