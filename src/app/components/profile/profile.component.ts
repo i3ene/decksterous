4b45aa599f1a6a89662615f150466b08c341fc7c
I3ene <benedikt.muell@t-online.de>
2022-11-18 16:53:02 +0100

    Implemented Profile Page for public view

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { RequestService } from '../../services/request/request.service';
 import { User } from '../../models/data/user.model';
 import { TokenService } from 'src/app/services/auth/token.service';
 import { StatsService } from 'src/app/services/stats.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -11,10 +12,23 @@ import { StatsService } from 'src/app/services/stats.service';
 })
 export class ProfileComponent implements OnInit {
   user: User = new User({ name: 'Loading...' });
+  id?: number;
 
-  constructor(private token: TokenService, private request: RequestService, public stats: StatsService) {}
+  constructor(private route: ActivatedRoute, private token: TokenService, private request: RequestService, public stats: StatsService) {
+    this.route.queryParams.subscribe(params => this.idChange(params['id']));
+  }
+
+  idChange(id?: number) {
+    this.id = id;
+    if (id) this.loadUser(id);
+    else this.loadToken();
+  }
 
   ngOnInit(): void {
+    if (!this.id) this.loadToken();
+  }
+
+  loadToken(): void {
     const data = this.token.getTokenData();
     if (data.id) this.loadUser(data.id);
   }
