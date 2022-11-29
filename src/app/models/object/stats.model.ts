34a0e81dbaac2efa89a63dc3aa308bef9434ba08
I3ene <benedikt.muell@t-online.de>
2022-11-29 11:10:53 +0100

    Adjusted level display to relative stats

--- src/app/models/object/stats.model.ts
+++ src/app/models/object/stats.model.ts
@@ -1,6 +1,24 @@
-export interface UserLevelStats {
+export class UserLevelStats {
+  xp: number;
   level: number;
   current: number;
   next: number;
   progress: number;
+
+  constructor() {
+    this.xp = 0;
+    this.level = 0;
+    this.current = 0;
+    this.next = 0;
+    this.progress = 0;
+  }
+
+  get levelCurrent(): number {
+    return this.xp - this.current;
+  }
+
+  get levelNext(): number {
+    return this.next - this.current;
+  }
 }
+
--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -10,20 +10,17 @@ export class StatsService {
   levelXpScale: number = 0.1;
 
   user(xp: number): UserLevelStats {
-    const stats: UserLevelStats = {
-      level: 0,
-      current: 0,
-      next: this.firstLevelXp,
-      progress: 0
-    };
-    while(xp >= stats.next) {
+    const stats: UserLevelStats = new UserLevelStats();
+    stats.xp = xp;
+    stats.next = this.firstLevelXp;
+    while(stats.xp >= stats.next) {
       stats.level++;
       let last = stats.next - stats.current;
       stats.current = stats.next;
       stats.next += last + (this.firstLevelXp * this.levelXpScale);
       stats.next = Number(stats.next.toFixed(0));
     }
-    stats.progress = (xp - stats.current) / (stats.next - stats.current);
+    stats.progress = (stats.xp - stats.current) / (stats.next - stats.current);
     return stats;
   }
 
