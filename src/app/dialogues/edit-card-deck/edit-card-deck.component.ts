e2bd2cd52b9f6e1bd33a5999e323266b4dba6fcc
I3ene <benedikt.muell@t-online.de>
2022-11-29 14:59:25 +0100

    Updated EditDeck Dialogue

--- src/app/dialogues/edit-card-deck/edit-card-deck.component.ts
+++ src/app/dialogues/edit-card-deck/edit-card-deck.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InventoryComponent } from 'src/app/components/inventory/inventory.component';
+import { CardDeck } from 'src/app/models/data/card.model';
 import { Item, ItemType } from 'src/app/models/data/item.model';
 
 @Component({
@@ -11,11 +12,14 @@ export class EditCardDeckComponent implements OnInit {
 
   @ViewChild(InventoryComponent, { static: true }) inventory!: InventoryComponent;
 
+  deck!: CardDeck;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
     this.inventory.items = (this.data.items as Item[]).filter(x => x.type == ItemType.CARD);
-    console.log(this.data.deck);
+    this.deck = Object.assign({}, this.data.deck);
+    this.deck.item = Object.assign({}, this.deck.item);
   }
 
 }
