a6ae06d36c0c92884769cc81545facfd3e73cb73
Benedikt Müll <benedikt.muell@zimmer-group.com>
2022-12-10 20:48:41 +0100

    Implemented Dashboard navigation

--- src/app/pages/navigation/home/home.component.ts
+++ src/app/pages/navigation/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/models/object/menu.model';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  items: MenuItem[] = [
+    { name: "lobbies", link: ["/navigation/lobby"], icon: "list" },
+    { name: "ranking", link: ["/navigation/ranking"], icon: "leaderboard" },
+    { name: "solo", link: ["/game"], icon: "style" },
+    { name: "profile", link: ["/navigation/profile"], icon: "person" },
+    { name: "inventory", link: ["/navigation/inventory"], icon: "category" }
+  ];
 }
