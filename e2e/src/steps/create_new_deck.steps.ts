7e1c5b0fccc039c3c91be8acd2ec681466f118c5
Oliver Saar <oli.saar123@gmail.com>
2022-12-11 17:26:48 +0100

    worked on tests

--- e2e/src/steps/create_new_deck.steps.ts
+++ e2e/src/steps/create_new_deck.steps.ts
@@ -1,53 +1,59 @@
 // Import the cucumber operators we need
-import { Before, Given, Then, When } from "@cucumber/cucumber";
-import { AppPage } from "../app.po";
-import { element, by, protractor, browser } from "protractor";
-import { expect } from "chai";
+import { Before, Given, Then, When } from '@cucumber/cucumber';
+import { AppPage } from '../app.po';
+import { by, element, protractor } from 'protractor';
+import { expect } from 'chai';
 
 let page: AppPage;
-let EC = protractor.ExpectedConditions;
 
 Before(() => {
   page = new AppPage();
 });
 
+const name: string = 'deckname';
+
 /** open the inventory page **/
 
-Given("Given I am signed in with username {string} and password {string}", async (x: string, y: string) => {
-  //TODO
+Given('Given I am signed in with username {string} and password {string}', async (un: string, pw: string) => {
+  const username = un;
+  const password = pw;
 });
 
-Then("I am on the {string} page", async (x: string) => {
-  //TODO
+Then('I am on the {string} page', async (x: string) => {
+  await page.navigateTo();
 });
 
-When("I press the {string} button", async (x: string) => {
-  //TODO
+When('I press the {string} button', async (x: string) => {
+  const inventoryButton = element(by.id(x));
+  inventoryButton.click();
 });
-
 
 /** enter valid data and save the operation **/
 
-When("I select {string}", async (x: string) => {
+When('I select {string}', async (x: string) => {
+  const selectNewDeck = element(by.id(x));
+  selectNewDeck.click();
+});
+
+Then('I enter a {string}', async (name) => {
+  const deckName = element(by.id('deckName'));
+  deckName.sendKeys(name);
+  expect(await deckName.getAttribute('value')).to.equal(name);
+});
+
+When('I press {string}', async (x: string) => {
+  const selectCards = element(by.id(x));
+  selectCards.click();
+});
+
+Then('I add cards to the deck', async () => {
   //TODO
 });
 
-Then("I enter a {string}", async (x: string) => {
-  //TODO
+Then('I am back again on the {string} page', async (x: string) => {
+  await page.navigateTo();
 });
 
-When("I press {string}", async (x: string) => {
-  //TODO
-});
-
-Then("I add cards to the deck", async () => {
-  //TODO
-});
-
-Then("I am back again on the {string} page", async (x: string) => {
-  //TODO
-});
-
-Then("I receive a {string} message", async (x: string) => {
+Then('I receive a {string} message', async (x: string) => {
   //TODO
 });
--- e2e/src/steps/trade_items.steps.ts
+++ e2e/src/steps/trade_items.steps.ts
@@ -1,8 +1,8 @@
 // Import the cucumber operators we need
-import { Before, Given, Then, When } from "@cucumber/cucumber";
-import { AppPage } from "../app.po";
-import { element, by, protractor, browser } from "protractor";
-import { expect } from "chai";
+import { Before, Given, Then, When } from '@cucumber/cucumber';
+import { AppPage } from '../app.po';
+import { browser, by, element, protractor } from 'protractor';
+import { expect } from 'chai';
 
 let page: AppPage;
 let EC = protractor.ExpectedConditions;
@@ -13,84 +13,96 @@ Before(() => {
 
 /** select a friend for trading **/
 
-Given("Given I am signed in with username {string} and password {string}", async (x: string, y: string) => {
-  //TODO
+Given('Given I am signed in with username {string} and password {string}', async (x: string, y: string) => {
+  const username: string = x;
+  const password: string = y;
 });
 
-Then("I am on the {string} page", async (x: string) => {
-  //TODO
+Then('I am on the {string} page', async (x: string) => {
+  await page.navigateTo();
 });
 
-When("I press the {string} button", async (x: string) => {
-  //TODO
+When('I press the {string} button', async (x: string) => {
+  const friendButton = element(by.id(x));
+  friendButton.click();
 });
 
-
-
-When("I select a friend listed on this page", async () => {
-  //TODO
+When('I select a friend listed on this page', async () => {
+  const friend = element(by.id('friend'));
+  friend.click();
 });
 
-Then("they will receive a {string}-request", async (x: string) => {
+Then('they will receive a {string}-request', async (x: string) => {
   //TODO
 });
-
 
 /** friend refuses the request **/
 
-Given("the friend is refusing the request", async () => {
-  //TODO
+Given('the friend is refusing the request', async () => {
+  const refuseButton = element(by.id('refuseButton'));
+  refuseButton.click();
+  expect(await refuseButton.isSelected()).to.true;
 });
 
-Then("I will receive a pop-up", async () => {
-  //TODO
+Then('I will receive a pop-up that says {string}', async (x: string) => {
+  const popup = element(by.id('popup'));
+  browser.wait(EC.presenceOf(popup), 5000);
+  expect(await popup.isPresent()).to.true;
+  expect(await popup.getText()).to.equal(x);
 });
 
-Then("And I get informed that the request was declined", async () => {
+Then('And I get informed that the request was declined', async () => {
   //TODO
 });
-
 
 /** friend accepts request **/
 
-Given("the friend is accepting the request", async () => {
+Given('the friend is accepting the request', async () => {
+  const acceptButton = element(by.id('acceptButton'));
+  acceptButton.click();
+  expect(await acceptButton.isSelected()).to.true;
+});
+
+Then('players will see the trade menu', async () => {
   //TODO
 });
 
-Then("players will see the trade menu", async () => {
+Then('I can select the cards I want to trade', async () => {
   //TODO
 });
 
-Then("I can select the cards I want to trade", async () => {
+Then('I see the cards my friend selects', async () => {
   //TODO
 });
-
-Then("I see the cards my friend selects", async () => {
-  //TODO
-});
-
 
 /** either player aborts the trade action **/
 
-Given("either player clicks on the {string} button", async (x: string) => {
-  //TODO
+Given('either player clicks on the {string} button', async (x: string) => {
+  const abortButton = element(by.id(x));
+  abortButton.click();
 });
 
-Then("both users receive a pop-up which says {string}", async (x: string) => {
-  //TODO
+Then('both users receive a pop-up which says {string}', async (x: string) => {
+  const popup = element(by.id('popup'));
+  browser.wait(EC.presenceOf(popup), 5000);
+  expect(await popup.isPresent()).to.true;
+  expect(await popup.getText()).to.equal(x);
 });
-
 
 /** both players click on the 'accept trade' button **/
 
-Given("both players click on the {string} button", async (x: string) => {
-  //TODO
+Given('both players click on the {string} button', async (x: string) => {
+  const acceptTradeButton = element(by.id(x));
+  acceptTradeButton.click();
 });
 
-Then("I will receive a pop-up which says {string}", async (x: string) => {
-  //TODO
+Then('I will receive a pop-up which says {string}', async (x: string) => {
+  const popup = element(by.id('popup'));
+  browser.wait(EC.presenceOf(popup), 5000);
+  expect(await popup.isPresent()).to.true;
+  expect(await popup.getText()).to.equal(x);
 });
 
-Then("I can find my new items in the inventory", async () => {
+Then('I can find my new items in the inventory', async () => {
   //TODO
 });
