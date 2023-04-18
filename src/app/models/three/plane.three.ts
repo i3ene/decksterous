857587bab64a69de34388fb7ff7296f4eabd4540
mfischer <fischer@econda.de>
2023-04-18 19:18:45 +0200

    Revert "added empty game scene and card object"
    This reverts commit 650aff8f0aa7011d1acd31bbc2273f64da93a17c.

--- src/app/models/three/plane.three.ts
+++ src/app/models/three/plane.three.ts
@@ -1,21 +1,18 @@
 import * as THREE from "three";
 import { BoxGeometry, BufferGeometry, Material, Mesh, MeshStandardMaterial, PlaneGeometry } from "three";
 
-export class PlaneThree extends THREE.Group {
-  geometry: BufferGeometry = new PlaneGeometry(1, 1, 1);
+export class PlaneThree {
+  geometry: BufferGeometry = new PlaneGeometry(10, 10, 1);
   material: Material = new MeshStandardMaterial({ color: 0xFFFFFF });
   mesh: Mesh;
 
   constructor(material?: Material) {
-    super();
     if (material) this.material = material;
     this.mesh = new Mesh(this.geometry, this.material);
 
     this.mesh.receiveShadow = true;
 
     this.mesh.rotateX(THREE.MathUtils.degToRad(-90));
-
-    this.add(this.mesh);
   }
 
 }
--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,8 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { GameScene } from 'src/app/logic/scenes/game.scene';
 import { TestScene } from 'src/app/logic/scenes/test.scene';
 import { ThreeLogic } from 'src/app/logic/three.logic';
-import { SocketConnection } from 'src/app/services/request/socket.connection';
 
 @Component({
   templateUrl: './game.component.html',
@@ -13,12 +11,9 @@ export class GamePage implements AfterViewInit {
 
   threeLogic!: ThreeLogic;
 
-  constructor(private socket: SocketConnection) {}
-
   ngAfterViewInit(): void {
     this.threeLogic = new ThreeLogic(this.canvasRef);
-    this.threeLogic.loadScene(new TestScene());
-    setTimeout(() => this.threeLogic.loadScene(new GameScene(this.socket)), 200);
+    this.threeLogic.registerScene(new TestScene());
   }
-
+  
 }
