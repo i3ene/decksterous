0808cd6e84a6bb1c96ab4c299a2dc72d6d7764e4
I3ene <benedikt.muell@t-online.de>
2023-04-28 09:21:47 +0200

    adjusted and added typings

---
+++ src/typings/three.d.ts
@@ -0,0 +1,40 @@
+import { Subject } from 'rxjs';
+import * as three from 'three';
+
+declare module 'three' {
+  interface Object3D {
+    _selected: boolean;
+    /**
+     * Selection attribute (from raycaster)
+     */
+    selected: boolean;
+    
+    /**
+     * If selectable
+     */
+    selectable: boolean;
+
+    _selecting: Subject<boolean>;
+    /**
+     * Event on selection change
+     */
+    selecting: Subject<boolean>
+
+    
+    /**
+     * Click attribute (from raycaster)
+     */
+    clicked: number;
+
+    /**
+     * If clickable
+     */
+    clickable: boolean;
+
+    _clicking: Subject<number>;
+    /**
+     * Event on click
+     */
+    clicking: Subject<number>;
+  }
+}
