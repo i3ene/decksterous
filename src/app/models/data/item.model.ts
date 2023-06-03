73f654bc87415955352fe38e8340909c1c2ef4b5
Oliver Saar <oli.saar123@gmail.com>
2023-06-03 16:28:02 +0200

    frontend model adjustments

--- src/app/models/data/item.model.ts
+++ src/app/models/data/item.model.ts
@@ -1,37 +1,211 @@
-import { Card, CardDeck } from "./card.model";
-import { InventoryItem } from "./inventory.model";
+import { ModelUtils } from "src/app/utils/model.util";
+import { Ability, Type } from "./card.model";
+import { ModelParsable } from "./object.model";
 
-export class Item {
-  id?: number;
+/* Interfaces */
+
+export interface IItemType {
+  readonly type: ItemType;
+}
+
+export interface IItem {
+  id: number;
   name: string;
   description: string;
   image?: any;
-  card?: Card;
-  cardDeck?: CardDeck;
-  inventoryItem?: InventoryItem;
+}
 
-  constructor(obj?: any, inventoryItem?: any) {
+export interface IDeck {
+  id: number;
+  maxCards: number;
+}
+
+export interface ICard {
+  id: number;
+  typeId: number;
+  health: number;
+  damage: number;
+  cost: number;
+
+}
+
+export interface IPack {
+  id: number;
+  rarity: number;
+}
+
+/* Abstracts */
+
+export abstract class AItem implements IItem {
+  id: number;
+  name: string;
+  description: string;
+  image?: any;
+
+  constructor(obj?: any) {
+    this.id = Number(obj?.id ?? 0);
     this.name = obj?.name ?? '';
     this.description = obj?.description ?? '';
-    if (obj?.id) this.id = Number(obj.id);
     if (obj?.image) this.image = obj.image;
-    if (obj?.card) this.card = new Card(obj.card, this);
-    if (obj?.cardDeck) this.cardDeck = new CardDeck(obj.cardDeck, this);
-    if (inventoryItem) this.inventoryItem = inventoryItem;
-    else if (obj?.inventoryItem) this.inventoryItem = new InventoryItem(obj.inventoryItem);
-    else if (obj?.InventoryItem) this.inventoryItem = new InventoryItem(obj.InventoryItem);
-    // TODO: Fix spelling mistake
-  }
-
-  get type(): ItemType {
-    if (this.card) return ItemType.CARD;
-    if (this.cardDeck) return ItemType.DECK;
-    return ItemType.ITEM;
   }
 }
 
+export abstract class AItemType {
+  itemId: number;
+  item?: Item;
+
+  constructor(obj?: any) {
+    this.itemId = obj?.itemId ?? 0;
+    if (obj?.item) this.item = new Item(obj?.item);
+  }
+}
+
+export abstract class ADeck extends AItemType implements IDeck {
+  id: number;
+  maxCards: number;
+  
+  constructor(obj?: any) {
+    super(obj);
+    this.id = Number(obj?.id ?? 0);
+    this.maxCards = Number(obj?.maxCards ?? 0);
+  }
+}
+
+export abstract class ACard extends AItemType implements ICard {
+  id: number;
+  typeId: number;
+  health: number;
+  damage: number;
+  cost: number;
+  
+  constructor(obj?: any) {
+    super(obj);
+    this.id = Number(obj?.id ?? 0);
+    this.typeId = Number(obj?.typeId ?? 0);
+    this.health = Number(obj?.health ?? 0);
+    this.damage = Number(obj?.damage ?? 0);
+    this.cost = Number(obj?.cost ?? 0);
+  }
+}
+
+export abstract class APack extends AItemType implements IPack {
+  id: number;
+  rarity: number;
+  
+  constructor(obj?: any) {
+    super(obj);
+    this.id = Number(obj?.id ?? 0);
+    this.rarity = Number(obj?.rarity ?? 0);
+  }
+}
+
+/* Classes */
+
+export class Item extends AItem implements IItemType {
+  readonly type: ItemType = ItemType.ITEM;
+
+  constructor(obj?: any) {
+    super(obj);
+  }
+}
+
+export class Deck extends ADeck {
+  constructor(obj?: any) {
+    super(obj);
+  }
+}
+
+export class Card extends ACard {
+  type: Type;
+  abilities?: Ability[];
+
+  constructor(obj?: any) {
+    super(obj);
+    this.type = new Type(obj?.type);
+    this.abilities = ModelUtils.parseArray(Ability, obj?.abilities);
+  }
+}
+
+export class Pack extends APack {
+  constructor(obj?: any) {
+    super(obj);
+  }
+}
+
+/* Instances */
+
+export class CardItem extends Item {
+  override readonly type: ItemType = ItemType.CARD;
+  card: Card;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.card = new Card(obj?.card);
+  }
+}
+
+export class DeckItem extends Item {
+  override readonly type: ItemType = ItemType.DECK;
+  deck: Deck;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.deck = new Deck(obj?.deck);
+  }
+}
+
+export class PackItem extends Item {
+  override readonly type: ItemType = ItemType.PACK;
+  pack: Pack;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.pack = new Pack(obj?.pack);
+  }
+}
+
+/* Factory */
+
+export class ItemFactory<T extends Item> {
+  type?: ModelParsable<T>;
+
+  constructor(type?: ModelParsable<T>) {
+    this.type = type;
+  }
+
+  parse(obj?: any) {
+    const construct = this.type ?? ItemFactory.getConstruct(ItemFactory.getType(obj));
+    return new construct(obj);
+  }
+
+  static getType(obj?: any) {
+    let type = ItemType.ITEM;
+    if (obj?.deck) type = ItemType.DECK;
+    if (obj?.card) type = ItemType.CARD;
+    if (obj?.pack) type = ItemType.PACK;
+    return type;
+  }
+
+  static getConstruct(type: ItemType) {
+    switch(type) {
+      case ItemType.ITEM: return Item;
+      case ItemType.CARD: return CardItem;
+      case ItemType.DECK: return DeckItem;
+      case ItemType.PACK: return PackItem;
+      default: return Item;
+    }
+  }
+}
+
+/* Type */
+
+export type ItemAny = Item & Partial<CardItem> & Partial<DeckItem> & Partial<PackItem>;
+
+/* Enum */
+
 export enum ItemType {
-  CARD = "Card",
-  DECK = "Deck",
-  ITEM = "Item"
+  ITEM,
+  CARD,
+  DECK,
+  PACK
 }
--- src/app/models/data/user.model.ts
+++ src/app/models/data/user.model.ts
@@ -1,17 +1,101 @@
-export class User {
-  id?: number;
+
+/* Interfaces */
+
+import { ModelUtils } from "src/app/utils/model.util";
+import { _Object } from "./object.model";
+
+export interface IInventory {
+  id: number;
+  userId?: number;
+}
+
+export interface ICredentials {
   name: string;
+  password: string;
+}
+
+export interface IUser extends ICredentials {
+  id: number;
   xp: number;
-  password?: string;
+  coins: number;
   mail?: string;
   avatar?: any;
+}
+
+export interface IFriends {
+  accepted?: User[];
+  invites?: User[];
+  requests?: User[];
+}
+
+/* Abstracts */
+
+export abstract class AInventory implements IInventory {
+  id: number;
+  userId?: number;
+  
+  constructor(obj?: any) {
+    this.id = Number(obj?.id ?? 0);
+    this.userId = Number(obj?.userId ?? 0);
+  }
+}
+
+export abstract class AUser implements IUser {
+  id: number;
+  xp: number;
+  coins: number;
+  name: string;
+  password: string;
+  mail?: string;
+  avatar?: any;
+  
+  constructor(obj?: any) {
+    this.id = Number(obj?.id ?? 0);
+    this.xp = Number(obj?.xp ?? 0);
+    this.coins = Number(obj?.coins ?? 0);
+    this.name = obj?.name ?? '';
+    this.password = obj?.password ?? '';
+    this.mail = obj?.mail ?? '';
+    this.avatar = obj?.avatar;
+  }
+}
+
+export abstract class AFriends implements IFriends {
+  accepted?: User[];
+  invites?: User[];
+  requests?: User[];
 
   constructor(obj?: any) {
-    this.name = obj?.name ?? '';
-    this.xp = obj?.xp ?? 0;
-    if (obj?.id) this.id = Number(obj.id);
-    if (obj?.password) this.password = obj.password;
-    if (obj?.mail) this.mail = obj.mail;
-    if (obj?.avatar) this.avatar = obj.avatar;
+    this.accepted = ModelUtils.parseArray(User, obj?.accepted);
+    this.invites = ModelUtils.parseArray(User, obj?.invites);
+    this.requests = ModelUtils.parseArray(User, obj?.requests);
+  }
+}
+
+/* Classes */
+
+export class Inventory extends AInventory {
+  objects?: _Object<any>[];
+  user?: User;
+
+  constructor(obj?: any) {
+    super(obj);
+    this.objects = ModelUtils.parseArray(_Object, obj?.objects);
+    if (obj) this.user = new User(obj?.user);
+  }
+}
+
+export class User extends AUser {
+  inventory?: Inventory;
+
+  constructor(obj?: any) {
+    super(obj);
+    if (obj) this.inventory = new Inventory(obj?.inventory);
+  }
+}
+
+export class Friends extends AFriends {
+  constructor(obj?: any) {
+    super(obj);
   }
 }
