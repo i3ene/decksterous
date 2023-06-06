a691fa5b8cfd19e6997b2efda905c30b7aba54a7
Mats Fischer <mfischer.ka@gmx.de>
2023-06-06 09:47:12 +0200

    added handling and logic for marketplace functions

--- api/src/middleware/object.middleware.ts
+++ api/src/middleware/object.middleware.ts
@@ -3,6 +3,7 @@ import {RequestOptionsData} from "../models/object/request.model";
 import {RequestUtils} from "../utils/request.util";
 import {_Object} from "../models/data/object.model";
 import {Marketplace} from "../models/data/marketplace.model";
+import { User } from "../models/data/user.model";
 
 export namespace ObjectMiddleware {
   export function isOwn(equal: boolean, options: RequestOptionsData) {
@@ -26,6 +27,27 @@ export namespace ObjectMiddleware {
       const price = req.body.price ?? 0;
       const result = await Marketplace.create({objectHash: data.hash, price: price} as any);
       req.data.addMessage(key + ' successfully added!', 200, result);
+
+      next();
+    }
+  }
+
+  export function buy(options: RequestOptionsData) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      const key = options.data?.key;
+      const data = RequestUtils.byAttribute(req.data, key) as Marketplace;
+
+      if (req.user == undefined) return res.status(500).send({ message: "No Buyer data available!" });
+      req.user = (await User.scope("inventory").findByPk(req.user.id))!;
+      if (req.user?.inventory == undefined) return res.status(500).send({ message: "No Inventory available!" });
+      if (req.user.coins < data.price) return res.status(400).send({ message: "Not enough coins to buy!" });
+      data.object = (await data.$get("object", { scope: "user" }))!;
+      if (data.object?.inventory?.user == undefined) return res.status(500).send({ message: "No Seller information available!" }); 
+
+      await req.user.decrement("coins", { by: data.price });
+      await data.object.inventory.user.increment("coins", { by: data.price });
+      await data.object.$set("inventory", req.user.inventory);
+      await data.destroy();
 
       next();
     }
--- src/app/pages/navigation/marketplace/marketplace.component.ts
+++ src/app/pages/navigation/marketplace/marketplace.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemAny } from 'src/app/models/data/item.model';
-import { Marketplace, _Object } from 'src/app/models/data/object.model';
+import { IMarketplace, Marketplace, _Object } from 'src/app/models/data/object.model';
 import { DataService } from 'src/app/services/data.service';
 import { RequestService } from 'src/app/services/request/request.service';
 
@@ -29,6 +29,24 @@ export class MarketplacePage implements OnInit {
 
   async loadMarketplace(ownItems: boolean) {
     this.items = await (ownItems ? this.data.self.marketplaceObjects : this.data.self.getOtherMarketplaceObjects());
+  }
+
+  async handleAction(object: _Object<ItemAny>, event: 'buy' | 'remove' | 'update') {
+    switch(event) {
+      case 'buy':
+        await this.data.self.buyMarketplaceObject(object.hash);
+        this.items = this.items.filter(x => x.hash != object.hash);
+        break;
+      case 'remove':
+        await this.data.self.removeMarketplaceObject(object.hash);
+        this.items = this.items.filter(x => x.hash != object.hash);
+        break;
+      case 'update':
+        const marketplace: IMarketplace = { objectHash: object.hash, price: object.marketplace?.price ?? 0 };
+        const payload = await this.data.self.updateMarketplaceObject(marketplace);
+        Object.assign(object.marketplace ?? {}, payload);
+        break;
+    }
   }
 
 }
